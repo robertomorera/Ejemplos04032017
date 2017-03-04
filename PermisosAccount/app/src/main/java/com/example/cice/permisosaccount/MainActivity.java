648afa4c35fd79bb65d1ab5cba404e932cb071cf
cice <cice@aula8PovMacN08.local>
2017-03-04 12:02:14 +0100

    version listar cuentas

--- PermisosAccount/app/src/main/java/com/example/cice/permisosaccount/MainActivity.java
+++ PermisosAccount/app/src/main/java/com/example/cice/permisosaccount/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.cice.permisosaccount;
 import android.Manifest;
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -10,6 +12,13 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +33,28 @@ public class MainActivity extends AppCompatActivity {
         }else{
             pedirPermisos();
         }
+
+        Button botonLista=(Button)findViewById(R.id.btn_listarCuentas);
+        botonLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Obtenemos la sharedPreferences.
+                SharedPreferences sp=getSharedPreferences("listasCuentasUsuario",Context.MODE_PRIVATE);
+                //Obtenemos el número de cuentas del usuario.
+                int numeroCuentas=sp.getInt("numeroCuentas",0);
+                String[] listaCuentas= new String[numeroCuentas];
+                for(int i=0;i<listaCuentas.length;i++){
+                    listaCuentas[i]=sp.getString("cuenta"+i,"test");
+                }
+                ArrayAdapter<String> listAdapter=new ArrayAdapter<String>(MainActivity.this,R.layout.fila,R.id.textView,listaCuentas);
+                ListView listView=(ListView)findViewById(R.id.listaCuentas);
+                listView.setAdapter(listAdapter);
+            }
+
+
+        });
+
+
 
     }
 
@@ -45,23 +76,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private String[] obtenerCorreosUsuario (){
-        String[] lista_correos=null;
+    private List<String> obtenerCorreosUsuario (){
+        List<String> lista_correos=null;
         Account[] lista_cuentas=null;
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.GET_ACCOUNTS)==PackageManager.PERMISSION_GRANTED){
             AccountManager accountManager=(AccountManager)getSystemService(ACCOUNT_SERVICE);
             lista_cuentas=accountManager.getAccounts();
+            SharedPreferences sharedPreferences=getSharedPreferences("listasCuentasUsuario", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor=sharedPreferences.edit();
+            int i=0;
             for(Account cuenta:lista_cuentas){
-                if(cuenta.type.equals("com.google")) {
+                 lista_correos=new ArrayList<String>();
+                 lista_correos.add(cuenta.name);
+                //Añadimos las cuentas en el array de String[]
+
+                /**if(cuenta.type.equals("com.google")) {
                     Log.d(getClass().getCanonicalName(), "Cuenta de correo de GMAIL " + cuenta.name);
                 }else if(cuenta.type.equals(("com.facebook.auth.login"))){
-                    4Log.d(getClass().getCanonicalName(), "Cuenta de FACEBOOK " + cuenta.name);
+                    Log.d(getClass().getCanonicalName(), "Cuenta de FACEBOOK " + cuenta.name);
 
                 }else{
                     Log.d(getClass().getCanonicalName(),"Cuenta (No google) tipo " +cuenta.type);
-                }
+                }**/
+                editor.putInt("numeroCuentas",lista_cuentas.length);
+                editor.putString("cuenta"+i,cuenta.name);
+                editor.commit();
+                i++;
             }
         }
+        //Creamos el sharedPreferences y metemos la lista de las cuentas.
+
+
 
         return lista_correos;
     }
